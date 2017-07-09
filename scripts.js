a2f9b7dbd96cd8972d23e9595b73239a54bb1692
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2017-07-09 13:35:24 -0300

    best fit with width

--- scripts.js
+++ scripts.js
@@ -3,12 +3,14 @@ $(function() {
         .css('flex-grow', 4)
         .each((i, e) => maxFontSizeToFit(e))
         .css('flex-grow', '');
+    $('section').each((i, element) => element.id = 'slide-' + i);
 });
 
 function maxFontSizeToFit(element) {
     var fitContent = $('<span />').html($(element).html());
     $(element).html('');
     var expectedHeight = $(element).height();
+    var expectedWidth = $(element).width();
     $(element).append(fitContent);
     var ratio = 1;
     var compare = (diff) => diff < 0;
@@ -24,6 +26,10 @@ function maxFontSizeToFit(element) {
     		element.css('color', 'red');
     		break;
         }
+    }
+    while(expectedWidth < fitContent.width()) {
+    	var fontSize = parseInt($(element).css('font-size')) - 1;
+    	$(element).css('font-size', fontSize + 'px');
     }
     $(element).html(fitContent.html());
 }
